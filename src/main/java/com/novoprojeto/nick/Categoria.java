1962191c3680168f292d4d3b206a7329e4425f64
Andre Said <andresaid123@hotmail.com>
2020-10-26 07:46:24 -0300

    Primeiro commit - projeto criado e iniciando Swagger

---
+++ src/main/java/com/novoprojeto/nick/Categoria.java
@@ -0,0 +1,15 @@
+package com.novoprojeto.nick;
+
+public class Categoria {
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
---
+++ src/main/java/com/novoprojeto/nick/ExampleResource.java
@@ -0,0 +1,27 @@
+package com.novoprojeto.nick;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("/hello")
+public class ExampleResource {
+
+  
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		return "hello";
+	}
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("outro-hello")
+    public Categoria outroHello() {
+    	Categoria categoria = new Categoria();
+    	categoria.setNome("Nome da categoria");
+    	return categoria;
+    }
+}
